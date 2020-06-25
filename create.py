c5e640d8de83e3b22fde234638398e1a8666b0f3
Lucas Song <lucas.song@LucasSong-MBPi.local>
2020-06-25 12:36:39 -0700

    adjust create.py, delete.py, get.py, list.py for roundid/questionid and update location in serverless.yml

--- create.py
+++ create.py
@@ -14,11 +14,12 @@ def main(event, context):
 
     # Prepare a dynamo db item.
     item = {
-        'userId': {"S": event.get('requestContext').get('identity').get('cognitoIdentityId')},
-        'noteId': {"S": str(uuid.uuid1())},
-        'content' : {"S": body["content"]},
-        'attachment': {"S": body["attachment"]},
-        'createdAt': {"S": datetime.date.today().strftime("yyyy-mm-dd")}
+        #'userId': {"S": event.get('requestContext').get('identity').get('cognitoIdentityId')},
+        'roundId': {"S": str(uuid.uuid1())},
+        'questionId': {"S": str(uuid.uuid1())},
+        'question' : {"S": body["content"]},
+        #'attachment': {"S": body["attachment"]},
+        #'createdAt': {"S": datetime.date.today().strftime("yyyy-mm-dd")}
     }
 
     # Add that field entry to the table
